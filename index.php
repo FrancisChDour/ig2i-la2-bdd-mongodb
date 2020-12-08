12795a227f330639ada46e1aa0c73ae155997dcb
FrancisChDour <fdourlensmonchy@gmail.com>
2020-12-08 11:49:55 +0100

    add form

--- index.php
+++ index.php
@@ -72,6 +72,24 @@
     </table>
 </div>
 
+<?php
+// connect to mongodb
+$m = new MongoClient();
+echo "Connection to database successfully";
+
+// select a database
+$db = $m->fdourlens;
+echo "Database mydb selected";
+$collection = $db->reception;
+echo "Collection selected succsessfully";
+$cursor = $collection->find();
+// iterate cursor to display title of documents
+
+foreach ($cursor as $document) {
+    echo $document["quantite_recue"] . "\n";
+}
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"></script>
