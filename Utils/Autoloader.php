e150181974de034c36a42539d89ef1cf87a54de1
Romain la terreur <dctime02@gmail.com>
2020-12-11 12:14:47 +0100

    fix Autoloader.php (a tester)

--- Utils/Autoloader.php
+++ Utils/Autoloader.php
@@ -13,6 +13,8 @@ class Autoloader
     {
         $class = str_replace('App', '', $class);
 //        $class = str_replace('\\', '/', $class);
-        require __DIR__. '/../' . str_replace('\\', '/', $class) . '.php';
+        $class =  __DIR__. '/../' . str_replace('\\', '/', $class) . '.php';
+        require str_replace('//', '/', $class);
+        var_dump(str_replace('//', '/', $class));
     }
 }
